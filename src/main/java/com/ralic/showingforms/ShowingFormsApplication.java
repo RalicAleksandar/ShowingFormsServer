d99c1902096a85592fe6dd5902916e6b9f307d35
Ralic <acaralic@gmail.com>
2018-10-03 00:19:48 +0200

    Initial commit

---
+++ src/main/java/com/ralic/showingforms/ShowingFormsApplication.java
@@ -0,0 +1,12 @@
+package com.ralic.showingforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowingFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowingFormsApplication.class, args);
+    }
+}
